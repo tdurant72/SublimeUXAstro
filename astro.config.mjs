b7020de1bb254a8b8509dd9b1ec0b801f6970807
Tyus Durant <contact@tyusdurant.com>
2024-04-30 17:03:29 -0400

    update of netifly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import { astroImageTools } from "astro-imagetools";
+
 const owner = "astrojs";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), astroImageTools],
+  integrations: [tailwind(), svelte()],
 });
